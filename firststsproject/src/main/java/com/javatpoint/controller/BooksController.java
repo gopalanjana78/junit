4aadab1a097ff60e0724bb11f76e0694fc40c142
Gopal Anjana <gopalanjana7878@gmail.com>
2022-09-28 04:25:07 -0700

    written test cases for crud

---
+++ firststsproject/src/main/java/com/javatpoint/controller/BooksController.java
@@ -0,0 +1,56 @@
+package com.javatpoint.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javatpoint.model.Books;
+import com.javatpoint.service.BooksService;
+
+@RestController
+
+public class BooksController {
+ @Autowired
+BooksService booksService;
+ @GetMapping("/book")
+
+private List<Books>getAllBooks()
+{
+	return booksService.getAllBooks();
+}
+
+@GetMapping("/book/{bookid}")
+
+private Books getBooks(@PathVariable("bookid")int bookid )
+{
+return booksService.getBooksById(bookid);
+}
+
+	@DeleteMapping("/book")
+	private void deleteBook(@RequestParam("bookid")int bookid)
+	{
+		booksService.delete(bookid);
+	}
+
+	@PostMapping("/books")
+	
+	private Books saveBook(@RequestBody Books books)
+	{
+		booksService.saveOrUpdate(books);
+		return books;
+	}
+	
+	@PutMapping("/books")
+	private Books update (@RequestBody Books books)
+	{
+		booksService.saveOrUpdate(books);
+		return books;
+	}
+}
